56625cf45e72ba59dc87782df6644b106d673996
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-05-20 10:45:03 +0530

    created method calls in messagesControler *the wrong way;

--- messages-app/src/messages/messages.controller.ts
+++ messages-app/src/messages/messages.controller.ts
@@ -1,19 +1,43 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('/messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DONT DO THIS IN A REAL APP
+    /* USE DEPENDENCY INJECTION */
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log('body', body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log('id', id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
---
+++ messages-app/src/messages/messages.service.ts
@@ -0,0 +1,23 @@
+import { MessageRepository } from './messages.repository';
+
+export class MessagesService {
+  messagesRepo: MessageRepository;
+
+  constructor() {
+    // Service is creating its own dependencies
+    /* !!!DONT DO THIS IN REAL APPS!!! */
+    this.messagesRepo = new MessageRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}

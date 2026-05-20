0fb0d78ac242553f1525ed1f8b547d77aef8b307
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-05-20 11:57:17 +0530

    created a computerModule, cpuModule, diskModule, powerModule; used DI to connect powerService to cpu and disk modules; connected cpu and disk modules to computerModule and used in computerController;

---
+++ dependency-injection/src/cpu/cpu.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+import { PowerService } from 'src/power/power.service';
+
+@Injectable()
+export class CpuService {
+  constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10W of power from the Power Service.');
+
+    this.powerService.supplyPower(10);
+
+    return a + b;
+  }
+}

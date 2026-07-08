73356e4bf774fb7539a2b24ec73c85634c9fa196
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-07-08 21:43:13 +0530

    added signin route to users controller; added signin method to auth service;

--- used-car-pricing-api/src/users/auth/auth.service.ts
+++ used-car-pricing-api/src/users/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { scrypt as _scrypt, randomBytes } from 'crypto';
 import { promisify } from 'util';
 
@@ -27,5 +31,21 @@ export class AuthService {
     return user;
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Incorrect password!');
+    }
+
+    return user;
+  }
 }

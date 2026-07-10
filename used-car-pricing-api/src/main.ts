fda07b42e064b24284c346b39d54b6488614bc92
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-07-10 18:30:47 +0530

    added signup, signin, signout to user controller;

--- used-car-pricing-api/src/main.ts
+++ used-car-pricing-api/src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import cookieSession from 'cookie-session';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieSession({ keys: ['qwerty'] }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- used-car-pricing-api/src/users/users.controller.ts
+++ used-car-pricing-api/src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  Session,
 } from '@nestjs/common';
 
 import { Serialize } from '../interceptors/serialize.interceptor';
@@ -25,14 +26,28 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }
+
   @Post('/signup')
-  signUp(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async signUp(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signout')
+  signout(@Session() session: any) {
+    session.userId = null;
   }
 
   @Get('/:id')

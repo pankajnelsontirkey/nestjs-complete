1759d685d1dd0c21331a7ab8fb2c1510664e1957
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-07-14 19:27:19 +0530

    add e2e tests for authentication system; move out ValidationPipe and CookieSession middleware from bootstrap method to AppModule;

--- used-car-pricing-api/src/main.ts
+++ used-car-pricing-api/src/main.ts
@@ -1,15 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import cookieSession from 'cookie-session';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieSession({ keys: ['qwerty'] }));
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();

2b6257ef17d119d9a136e999a93bb95e7bd55095
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-07-17 21:19:15 +0530

    created get-estimate.dto; added getEstimate handler to ReportsController;

--- used-car-pricing-api/src/reports/reports.controller.ts
+++ used-car-pricing-api/src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 
@@ -16,6 +18,7 @@ import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 @Serialize(ReportDto)
@@ -32,5 +35,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(parseInt(id), body.approved);
+  }
+
+  @Get('')
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
   }
 }

3789c7feac32aad941db8270b75ee4e7a55e2242
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-07-17 17:08:43 +0530

    added createReport controller; added create method in ReportsService; create Report.dto with transform for User enttity to userId field;

--- used-car-pricing-api/src/reports/reports.controller.ts
+++ used-car-pricing-api/src/reports/reports.controller.ts
@@ -1,15 +1,21 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { CreateReportDto } from './dtos/create-report.dto';
-import { ReportsService } from './reports.service';
+
 import { AuthGuard } from '../guards/auth.guard';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
+@Serialize(ReportDto)
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
   @Post('')
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@CurrentUser() user: User, @Body() body: CreateReportDto) {
+    return this.reportsService.create(body, user);
   }
 }

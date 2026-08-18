c6c0c80d42dc8783c2e5f21d0ebe43448f7f4c70
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-08-18 20:12:32 +0530

    update typeorm production config to add database url for heroku;

--- used-car-pricing-api/src/config/typeorm.config.ts
+++ used-car-pricing-api/src/config/typeorm.config.ts
@@ -7,10 +7,23 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    const isProd = this.configService.get<string>('NODE_ENV') === 'production';
+
+    if (isProd) {
+      return {
+        type: 'postgres',
+        url: this.configService.get<string>('DATABASE_URL'),
+        entities: ['**/*.entity.js'],
+        ssl: { rejectUnauthorized: false },
+      };
+    }
+
     return {
       type: 'better-sqlite3',
-      synchronize: this.configService.get<string>('NODE_ENV') !== 'production',
       database: this.configService.get<string>('DB_NAME'),
+      synchronize: this.configService.get<string>('NODE_ENV') !== 'production',
+      migrationsRun: true,
+      entities: ['**/*.entity.ts'],
       autoLoadEntities: true,
     };
   }

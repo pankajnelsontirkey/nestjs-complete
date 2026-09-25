b721511b9d621925716bcae8e6dae13bf2180b6d
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-09-25 16:49:33 +0530

    added DataSource options for both development and production;

--- used-car-pricing-api/src/data-source.ts
+++ used-car-pricing-api/src/data-source.ts
@@ -3,16 +3,33 @@ import { DataSource } from 'typeorm';
 
 import { Report } from './reports/report.entity';
 import { User } from './users/user.entity';
+import { DataSourceOptions } from 'typeorm/browser';
 dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
-if (!process.env.DB_NAME) {
-  throw new Error('DB_NAME not set in env file');
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-export default new DataSource({
-  type: 'better-sqlite3',
-  database: process.env.DB_NAME as string,
-  entities: [User, Report],
-  migrations: ['src/migrations/*.ts'],
-  synchronize: false,
-});
+const options: DataSourceOptions = isProd
+  ? {
+      type: 'postgres',
+      url: (() => {
+        if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not set');
+        return process.env.DATABASE_URL;
+      })(),
+      ssl: { rejectUnauthorized: false },
+      poolSize: 5,
+      entities: [User, Report],
+      migrations: ['src/migrations/*.ts'],
+      synchronize: false,
+    }
+  : {
+      type: 'better-sqlite3',
+      database: (() => {
+        if (!process.env.DB_NAME) throw new Error('DB_NAME not set');
+        return process.env.DB_NAME;
+      })(),
+      entities: [User, Report],
+      migrations: ['src/migrations/*.ts'],
+      synchronize: false,
+    };
+
+export default new DataSource(options);
